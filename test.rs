1a957d49f29b422d2b83edfbf2faeaf857e6e489
Dominik Spitzli <dominik@spitzli.dev>
2022-09-25 14:43:22 +0200

    hello_cargo

---
+++ test.rs
@@ -0,0 +1,14 @@
+//hello world
+fn main() {
+    println!("Hello, world!");
+}
+
+//console rickroll
+fn main() {
+    println!("Never gonna give you up");
+    println!("Never gonna let you down");
+    println!("Never gonna run around and desert you");
+    println!("Never gonna make you cry");
+    println!("Never gonna say goodbye");
+    println!("Never gonna tell a lie and hurt you");
+}
